34ba748c1a391c58f0f2e6df2f2f5f3c5fae6647
prospero78su <prospero.78.su@gmail.com>
2021-04-06 21:29:15 +0300

    Рефакторинг

--- tv/window.go
+++ tv/window.go
@@ -73,46 +73,46 @@ func NewWindow(x types.ACoordX, y types.ACoordY,
 	return wnd
 }
 
-func (wnd *TWindow) buttonCount() (left, right int) {
-	if wnd.buttons&ButtonClose == ButtonClose {
+func (sf *TWindow) buttonCount() (left, right int) {
+	if sf.buttons&ButtonClose == ButtonClose {
 		right += 1
 	}
-	if wnd.buttons&ButtonMaximize == ButtonMaximize {
+	if sf.buttons&ButtonMaximize == ButtonMaximize {
 		left += 1
 	}
-	if wnd.buttons&ButtonBottom == ButtonBottom {
+	if sf.buttons&ButtonBottom == ButtonBottom {
 		left += 1
 	}
 
 	return left, right
 }
 
-func (wnd *TWindow) drawFrame() {
+func (sf *TWindow) drawFrame() {
 	PushAttributes()
 	defer PopAttributes()
 
 	var bs BorderStyle
 	switch {
-	case wnd.border == BorderAuto:
-		if wnd.inactive {
+	case sf.border == BorderAuto:
+		if sf.inactive {
 			bs = BorderThin
 		} else {
 			bs = BorderThick
 		}
-	case wnd.border == BorderNone:
+	case sf.border == BorderNone:
 	default:
-		bs = wnd.border
+		bs = sf.border
 	}
 
-	DrawFrame(wnd.pos.GetX(), wnd.pos.GetY(), int(wnd.width.Get()), int(wnd.height), bs)
+	DrawFrame(sf.pos.GetX(), sf.pos.GetY(), int(sf.width.Get()), int(sf.height), bs)
 }
 
-func (wnd *TWindow) drawTitle() {
+func (sf *TWindow) drawTitle() {
 	PushAttributes()
 	defer PopAttributes()
 
-	lb, rb := wnd.buttonCount()
-	maxw := wnd.width.Get() - 2
+	lb, rb := sf.buttonCount()
+	maxw := sf.width.Get() - 2
 	xshift := 1
 	if lb > 0 {
 		lbSize := lb + 2 + 1
@@ -126,16 +126,16 @@ func (wnd *TWindow) drawTitle() {
 		return
 	}
 
-	fitTitle := wnd.title
+	fitTitle := sf.title
 	rawText := UnColorizeText(fitTitle)
 	if xs.Len(rawText) > int(maxw) {
 		fitTitle = SliceColorized(fitTitle, 0, int(maxw)-3) + "..."
 	}
-	DrawText(wnd.pos.GetX()+types.ACoordX(xshift), wnd.pos.GetY(), fitTitle)
+	DrawText(sf.pos.GetX()+types.ACoordX(xshift), sf.pos.GetY(), fitTitle)
 }
 
-func (wnd *TWindow) drawButtons() {
-	lb, rb := wnd.buttonCount()
+func (sf *TWindow) drawButtons() {
+	lb, rb := sf.buttonCount()
 	if lb+rb == 0 {
 		return
 	}
@@ -148,76 +148,76 @@ func (wnd *TWindow) drawButtons() {
 
 	// draw close button (rb can be either 1 or 0)
 	if rb != 0 {
-		pos := wnd.pos.GetX() + types.ACoordX(int(wnd.width.Get())-rb-2)
-		putCharUnsafe(pos, wnd.pos.GetY(), cOpenB)
-		putCharUnsafe(pos+1, wnd.pos.GetY(), cClose)
-		putCharUnsafe(pos+2, wnd.pos.GetY(), cCloseB)
+		pos := sf.pos.GetX() + types.ACoordX(int(sf.width.Get())-rb-2)
+		putCharUnsafe(pos, sf.pos.GetY(), cOpenB)
+		putCharUnsafe(pos+1, sf.pos.GetY(), cClose)
+		putCharUnsafe(pos+2, sf.pos.GetY(), cCloseB)
 	}
 
 	if lb > 0 {
-		posX := wnd.pos.GetX() + 1
-		putCharUnsafe(posX, wnd.pos.GetY(), cOpenB)
+		posX := sf.pos.GetX() + 1
+		putCharUnsafe(posX, sf.pos.GetY(), cOpenB)
 		posX += 1
-		if wnd.buttons&ButtonBottom == ButtonBottom {
-			putCharUnsafe(posX, wnd.pos.GetY(), cBottom)
+		if sf.buttons&ButtonBottom == ButtonBottom {
+			putCharUnsafe(posX, sf.pos.GetY(), cBottom)
 			posX += 1
 		}
-		if wnd.buttons&ButtonMaximize == ButtonMaximize {
-			putCharUnsafe(posX, wnd.pos.GetY(), cMax)
+		if sf.buttons&ButtonMaximize == ButtonMaximize {
+			putCharUnsafe(posX, sf.pos.GetY(), cMax)
 			posX += 1
 		}
-		putCharUnsafe(posX, wnd.pos.GetY(), cCloseB)
+		putCharUnsafe(posX, sf.pos.GetY(), cCloseB)
 	}
 }
 
 // Draw repaints the control on the screen
-func (wnd *TWindow) Draw() {
+func (sf *TWindow) Draw() {
 	WindowManager().BeginUpdate()
 	defer WindowManager().EndUpdate()
 	PushAttributes()
 	defer PopAttributes()
 
-	fg, bg := RealColor(wnd.fg, wnd.Style(), ColorViewText), RealColor(wnd.bg, wnd.Style(), ColorViewBack)
+	fg, bg := RealColor(sf.fg, sf.Style(), ColorViewText), RealColor(sf.bg, sf.Style(), ColorViewBack)
 	SetBackColor(bg)
 
-	FillRect(wnd.pos.GetX(), wnd.pos.GetY(), int(wnd.width.Get()), int(wnd.height), ' ')
+	FillRect(sf.pos.GetX(), sf.pos.GetY(), int(sf.width.Get()), int(sf.height), ' ')
 
-	wnd.DrawChildren()
+	sf.DrawChildren()
 
 	SetBackColor(bg)
 	SetTextColor(fg)
 
-	wnd.drawFrame()
-	wnd.drawTitle()
-	wnd.drawButtons()
+	sf.drawFrame()
+	sf.drawTitle()
+	sf.drawButtons()
 }
 
 // HitTest returns type of a Window region at a given screen coordinates. The
 // method is used to detect if a mouse cursor on a window border or outside,
 // which window icon is under cursor etc
-func (c *TWindow) HitTest(x types.ACoordX, y types.ACoordY) HitResult {
-	if x > c.pos.GetX() && x < c.pos.GetX()+types.ACoordX(c.width.Get()-1) &&
-		y > c.pos.GetY() && y < c.pos.GetY()+types.ACoordY(c.height-1) {
+func (sf *TWindow) HitTest(x types.ACoordX, y types.ACoordY) HitResult {
+	if x > sf.pos.GetX() && x < sf.pos.GetX()+types.ACoordX(sf.width.Get()-1) &&
+		y > sf.pos.GetY() && y < sf.pos.GetY()+types.ACoordY(sf.height-1) {
 		return HitInside
 	}
 
 	hResult := HitOutside
 
 	switch {
-	case x == c.pos.GetX() && y == c.pos.GetY():
+	case x == sf.pos.GetX() && y == sf.pos.GetY():
 		hResult = HitTopLeft
-	case x == c.pos.GetX()+types.ACoordX(c.width.Get()-1) && y == c.pos.GetY():
+	case x == sf.pos.GetX()+types.ACoordX(sf.width.Get()-1) && y == sf.pos.GetY():
 		hResult = HitTopRight
-	case x == c.pos.GetX() && y == c.pos.GetY()+types.ACoordY(c.height-1):
+	case x == sf.pos.GetX() && y == sf.pos.GetY()+types.ACoordY(sf.height-1):
 		hResult = HitBottomLeft
-	case x == c.pos.GetX()+types.ACoordX(c.width.Get()-1) && y == c.pos.GetY()+types.ACoordY(c.height-1):
+	case x == sf.pos.GetX()+types.ACoordX(sf.width.Get()-1) && y == sf.pos.GetY()+types.ACoordY(sf.height-1):
 		hResult = HitBottomRight
-	case x == c.pos.GetX() && y > c.pos.GetY() && y < c.pos.GetY()+types.ACoordY(c.height-1):
+	case x == sf.pos.GetX() && y > sf.pos.GetY() && y < sf.pos.GetY()+types.ACoordY(sf.height-1):
 		hResult = HitLeft
-	case x == c.pos.GetX()+types.ACoordX(c.width.Get()-1) && y > c.pos.GetY() && y < c.pos.GetY()+types.ACoordY(c.height-1):
+	case x == sf.pos.GetX()+types.ACoordX(sf.width.Get()-1) && y > sf.pos.GetY() && y < sf.pos.GetY()+types.ACoordY(sf.height-1):
 		hResult = HitRight
-	case y == c.pos.GetY() && x > c.pos.GetX() && x < c.pos.GetX()+types.ACoordX(c.width.Get()-1):
-		lb, rb := c.buttonCount()
+	case y == sf.pos.GetY() && x > sf.pos.GetX() && x < sf.pos.GetX()+types.ACoordX(sf.width.Get()-1):
+		lb, rb := sf.buttonCount()
 		fromL, fromR := lb, rb
 		if lb > 0 {
 			fromL += 2
@@ -225,21 +225,21 @@ func (c *TWindow) HitTest(x types.ACoordX, y types.ACoordY) HitResult {
 		if rb > 0 {
 			fromR += 2
 		}
-		if x > c.pos.GetX()+types.ACoordX(fromL) && x < c.pos.GetX()+types.ACoordX(int(c.width.Get())-fromR) {
+		if x > sf.pos.GetX()+types.ACoordX(fromL) && x < sf.pos.GetX()+types.ACoordX(int(sf.width.Get())-fromR) {
 			hResult = HitTop
 		} else {
 			hResult = HitTop
-			if c.buttons&ButtonClose == ButtonClose && rb != 0 && x == c.pos.GetX()+types.ACoordX(c.width.Get()-2) {
+			if sf.buttons&ButtonClose == ButtonClose && rb != 0 && x == sf.pos.GetX()+types.ACoordX(sf.width.Get()-2) {
 				hResult = HitButtonClose
-			} else if lb != 0 && x > c.pos.GetX()+1 && x < c.pos.GetX()+types.ACoordX(2+lb) {
-				dx := x - c.pos.GetX() - 2
+			} else if lb != 0 && x > sf.pos.GetX()+1 && x < sf.pos.GetX()+types.ACoordX(2+lb) {
+				dx := x - sf.pos.GetX() - 2
 				hitRes := []HitResult{HitTop, HitTop}
 				pos := 0
-				if c.buttons&ButtonBottom == ButtonBottom {
+				if sf.buttons&ButtonBottom == ButtonBottom {
 					hitRes[pos] = HitButtonBottom
 					pos += 1
 				}
-				if c.buttons&ButtonMaximize == ButtonMaximize {
+				if sf.buttons&ButtonMaximize == ButtonMaximize {
 					hitRes[pos] = HitButtonMaximize
 					// pos += 1
 				}
@@ -248,17 +248,17 @@ func (c *TWindow) HitTest(x types.ACoordX, y types.ACoordY) HitResult {
 				}
 			}
 		}
-	case y == c.pos.GetY()+types.ACoordY(c.height-1) &&
-		x > c.pos.GetX() &&
-		x < c.pos.GetX()+types.ACoordX(c.width.Get()-1):
+	case y == sf.pos.GetY()+types.ACoordY(sf.height-1) &&
+		x > sf.pos.GetX() &&
+		x < sf.pos.GetX()+types.ACoordX(sf.width.Get()-1):
 		hResult = HitBottom
 	}
 
 	if hResult != HitOutside {
-		if c.immovable && hResult == HitTop {
+		if sf.immovable && hResult == HitTop {
 			hResult = HitInside
 		}
-		if c.fixedSize &&
+		if sf.fixedSize &&
 			(hResult == HitBottom || hResult == HitLeft ||
 				hResult == HitRight || hResult == HitTopLeft ||
 				hResult == HitTopRight || hResult == HitBottomRight ||
@@ -270,28 +270,28 @@ func (c *TWindow) HitTest(x types.ACoordX, y types.ACoordY) HitResult {
 	return hResult
 }
 
-func (c *TWindow) ProcessEvent(ev Event) bool {
+func (sf *TWindow) ProcessEvent(ev Event) bool {
 	switch ev.Type {
 	case EventMove:
-		c.PlaceChildren()
+		sf.PlaceChildren()
 	case EventResize:
-		c.ResizeChildren()
-		c.PlaceChildren()
+		sf.ResizeChildren()
+		sf.PlaceChildren()
 	case EventClose:
-		if c.onClose != nil {
-			if !c.onClose(ev) {
+		if sf.onClose != nil {
+			if !sf.onClose(ev) {
 				return false
 			}
 		}
 		return true
 	case EventKey:
 		if ev.Key == term.KeyTab || ev.Key == term.KeyArrowUp || ev.Key == term.KeyArrowDown {
-			if SendEventToChild(c, ev) {
+			if SendEventToChild(sf, ev) {
 				return true
 			}
 
-			aC := ActiveControl(c)
-			nC := NextControl(c, aC, ev.Key != term.KeyArrowUp)
+			aC := ActiveControl(sf)
+			nC := NextControl(sf, aC, ev.Key != term.KeyArrowUp)
 
 			var clipped IControl
 
@@ -322,134 +322,134 @@ func (c *TWindow) ProcessEvent(ev Event) bool {
 			}
 			return true
 		}
-		if SendEventToChild(c, ev) {
+		if SendEventToChild(sf, ev) {
 			return true
 		}
-		if c.onKeyDown != nil {
-			return c.onKeyDown.fn(ev, c.onKeyDown.data)
+		if sf.onKeyDown != nil {
+			return sf.onKeyDown.fn(ev, sf.onKeyDown.data)
 		}
 		return false
 	default:
 		if ev.Type == EventMouse && ev.Key == term.MouseLeft {
-			DeactivateControls(c)
+			DeactivateControls(sf)
 		}
-		return SendEventToChild(c, ev)
+		return SendEventToChild(sf, ev)
 	}
 
 	return false
 }
 
 // OnClose sets the callback that is called when the Window is about to destroy
-func (w *TWindow) OnClose(fn func(Event) bool) {
-	w.onClose = fn
+func (sf *TWindow) OnClose(fn func(Event) bool) {
+	sf.onClose = fn
 }
 
 // OnKeyDown sets the callback that is called when a user presses a key
 // while the Window is active
-func (w *TWindow) OnKeyDown(fn func(Event, interface{}) bool, data interface{}) {
+func (sf *TWindow) OnKeyDown(fn func(Event, interface{}) bool, data interface{}) {
 	if fn == nil {
-		w.onKeyDown = nil
+		sf.onKeyDown = nil
 	} else {
-		w.onKeyDown = &keyDownCb{data: data, fn: fn}
+		sf.onKeyDown = &keyDownCb{data: data, fn: fn}
 	}
 }
 
 // OnScreenResize sets the callback that is called when size of terminal changes
-func (w *TWindow) OnScreenResize(fn func(Event)) {
-	w.onScreenResize = fn
+func (sf *TWindow) OnScreenResize(fn func(Event)) {
+	sf.onScreenResize = fn
 }
 
 // Border returns the default window border
-func (w *TWindow) Border() BorderStyle {
-	return w.border
+func (sf *TWindow) Border() BorderStyle {
+	return sf.border
 }
 
 // SetBorder changes the default window border
-func (w *TWindow) SetBorder(border BorderStyle) {
-	w.border = border
+func (sf *TWindow) SetBorder(border BorderStyle) {
+	sf.border = border
 }
 
 // SetMaximized opens the view to full screen or restores its
 // previous size
-func (w *TWindow) SetMaximized(maximize bool) {
-	if maximize == w.maximized {
+func (sf *TWindow) SetMaximized(maximize bool) {
+	if maximize == sf.maximized {
 		return
 	}
 
 	if maximize {
-		x, y := w.pos.Get()
-		w.posOrig.X().Set(x)
-		w.posOrig.Y().Set(y)
-		w.origWidth, w.origHeight = w.Size()
-		w.maximized = true
-		w.SetPos(0, 0)
+		x, y := sf.pos.Get()
+		sf.posOrig.X().Set(x)
+		sf.posOrig.Y().Set(y)
+		sf.origWidth, sf.origHeight = sf.Size()
+		sf.maximized = true
+		sf.SetPos(0, 0)
 		width, height := ScreenSize()
-		w.SetSize(width, height)
+		sf.SetSize(width, height)
 	} else {
-		w.maximized = false
-		w.SetPos(w.posOrig.GetX(), w.posOrig.GetY())
-		w.SetSize(w.origWidth, w.origHeight)
+		sf.maximized = false
+		sf.SetPos(sf.posOrig.GetX(), sf.posOrig.GetY())
+		sf.SetSize(sf.origWidth, sf.origHeight)
 	}
-	w.ResizeChildren()
-	w.PlaceChildren()
+	sf.ResizeChildren()
+	sf.PlaceChildren()
 }
 
 // Maximized returns if the view is in full screen mode
-func (w *TWindow) Maximized() bool {
-	return w.maximized
+func (sf *TWindow) Maximized() bool {
+	return sf.maximized
 }
 
 // Visible returns if the window must be drawn on the screen
-func (w *TWindow) Visible() bool {
-	return !w.hidden
+func (sf *TWindow) Visible() bool {
+	return !sf.hidden
 }
 
 // SetVisible allows to temporarily remove the window from screen
 // and show it later without reconstruction
-func (w *TWindow) SetVisible(visible bool) {
-	if w.hidden != visible {
+func (sf *TWindow) SetVisible(visible bool) {
+	if sf.hidden != visible {
 		return
 	}
 
-	w.hidden = !visible
-	if w.hidden {
-		w.SetModal(false)
-		if WindowManager().topWindow() == w {
+	sf.hidden = !visible
+	if sf.hidden {
+		sf.SetModal(false)
+		if WindowManager().topWindow() == sf {
 			WindowManager().moveActiveWindowToBottom()
 		}
 	} else {
-		WindowManager().activateWindow(w)
+		WindowManager().activateWindow(sf)
 	}
 }
 
 // Movable returns if the Window can be moved with mouse or keyboard
-func (w *TWindow) Movable() bool {
-	return !w.immovable
+func (sf *TWindow) Movable() bool {
+	return !sf.immovable
 }
 
 // Sizable returns if size of the Window can be changed with mouse or keyboard
-func (w *TWindow) Sizable() bool {
-	return !w.fixedSize
+func (sf *TWindow) Sizable() bool {
+	return !sf.fixedSize
 }
 
 // SetMovable turns on and off ability to change Window position with mouse
 // or keyboard
-func (w *TWindow) SetMovable(movable bool) {
-	w.immovable = !movable
+func (sf *TWindow) SetMovable(movable bool) {
+	sf.immovable = !movable
 }
 
 // SetSizable turns on and off ability to change Window size with mouse
 // or keyboard
-func (w *TWindow) SetSizable(sizable bool) {
-	w.fixedSize = !sizable
+func (sf *TWindow) SetSizable(sizable bool) {
+	sf.fixedSize = !sizable
 }
 
 // TitleButtons returns a set of buttons shown in the Window title bar
-func (w *TWindow) TitleButtons() ViewButton {
-	return w.buttons
+func (sf *TWindow) TitleButtons() ViewButton {
+	return sf.buttons
 }
 
 // SetTitleButtons sets the title bar buttons available for a user
-func (w *TWindow) SetTitleButtons(buttons ViewButton) {
-	w.buttons = buttons
+func (sf *TWindow) SetTitleButtons(buttons ViewButton) {
+	sf.buttons = buttons
 }
